87d7ae7500875a32fe45affb742aed70dbf29fcd
Luis Rovirosa <luisrovirosa@gmail.com>
2017-10-09 19:49:28 +0200

    refactor: reduce duplicity

--- src/Song.php
+++ src/Song.php
@@ -7,12 +7,10 @@ class Song
     public function song(): array
     {
         $sentences = [];
-        for ($i = 99; $i >= 1; $i--) {
+        for ($i = 99; $i >= 0; $i--) {
             $sentences[] = $this->bottlesOnTheWall($i);
             $sentences[] = $this->action($i) . ", " . $this->bottles($i - 1) . " of beer on the wall.";
         }
-        $sentences[] = ucfirst($this->bottles(0)) . " of beer on the wall, " . $this->bottles(0) . " of beer.";
-        $sentences[] = $this->action(0) . ", " . $this->bottles(99) . " of beer on the wall.";
         return $sentences;
     }
 
@@ -25,7 +23,7 @@ class Song
 
     public function bottlesOnTheWall(int $numberOfBottles): string
     {
-        return $this->bottles($numberOfBottles) . " of beer on the wall, " . $this->bottles($numberOfBottles) . " of beer.";
+        return ucfirst($this->bottles($numberOfBottles)) . " of beer on the wall, " . $this->bottles($numberOfBottles) . " of beer.";
     }
 
     private function bottles(int $numberOfBottles): string
@@ -34,6 +32,8 @@ class Song
             return "no more bottles";
         } elseif ($numberOfBottles === 1) {
             return "1 bottle";
+        } elseif ($numberOfBottles === -1) {
+            return "99 bottles";
         }
         return "$numberOfBottles bottles";
     }
