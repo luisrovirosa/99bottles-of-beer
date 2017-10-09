f298bfbffa1b7bf7d30476d5e9433c125ccaf43f
Luis Rovirosa <luisrovirosa@gmail.com>
2017-10-09 19:42:05 +0200

    refactor: reduce duplicity

--- src/Song.php
+++ src/Song.php
@@ -9,9 +9,9 @@ class Song
         $sentences = [];
         for ($i = 99; $i >= 2; $i--) {
             $sentences[] = $this->bottlesOnTheWall($i);
-            $sentences[] = "Take one down and pass it around, " . $this->bottles($i-1) . " of beer on the wall.";
+            $sentences[] = "Take one down and pass it around, " . $this->bottles($i - 1) . " of beer on the wall.";
         }
-        $sentences[] = $this->bottles(1) . " of beer on the wall, 1 bottle of beer.";
+        $sentences[] = $this->bottles(1) . " of beer on the wall, " . $this->bottles(1) . " of beer.";
         $sentences[] = "Take one down and pass it around, no more bottles of beer on the wall.";
         $sentences[] = "No more bottles of beer on the wall, no more bottles of beer.";
         $sentences[] = "Go to the store and buy some more, 99 bottles of beer on the wall.";
@@ -20,7 +20,7 @@ class Song
 
     public function sing()
     {
-        foreach ($this->song() as $sentence){
+        foreach ($this->song() as $sentence) {
             echo "$sentence\n";
         }
     }
@@ -32,7 +32,7 @@ class Song
 
     private function bottles(int $numberOfBottles): string
     {
-        if ($numberOfBottles === 1){
+        if ($numberOfBottles === 1) {
             return "1 bottle";
         }
         return "$numberOfBottles bottles";
