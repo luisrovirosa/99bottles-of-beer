7d76e69cc9337697b8fccb6cca3941a2bbff7a27
Luis Rovirosa <luisrovirosa@gmail.com>
2017-10-09 19:20:51 +0200

    the_second_sentence_is_correct

--- src/Song.php
+++ src/Song.php
@@ -1,11 +1,14 @@
 <?php
+
 namespace LuisRovirosa;
 
 class Song
 {
-
-    public function song():array
+    public function song(): array
     {
-        return ['99 bottles of beer on the wall, 99 bottles of beer.'];
+        return [
+            '99 bottles of beer on the wall, 99 bottles of beer.',
+            'Take one down and pass it around, 98 bottles of beer on the wall.'
+        ];
     }
 }
--- tests/Song99BottlesTest.php
+++ tests/Song99BottlesTest.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace LuisRovirosa;
+
 use PHPUnit\Framework\TestCase;
 
 class Song99BottlesTest extends TestCase
@@ -9,5 +11,12 @@ class Song99BottlesTest extends TestCase
     {
         $song = new Song();
         $this->assertEquals('99 bottles of beer on the wall, 99 bottles of beer.', $song->song()[0]);
+    }
+
+    /** @test */
+    public function the_second_sentence_is_correct()
+    {
+        $song = new Song();
+        $this->assertEquals('Take one down and pass it around, 98 bottles of beer on the wall.', $song->song()[1]);
     }
 }
